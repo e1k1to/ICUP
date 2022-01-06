a47237a088e61d24756b0912455f075060e145fc
eiki <eiki@DESKTOP-Q5UC1GU>
2022-01-06 15:55:08 -0300

    Adicionar arquivos de projeto.

---
+++ boaui/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace boaui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.one = new System.Windows.Forms.CheckBox();
+            this.two = new System.Windows.Forms.CheckBox();
+            this.four = new System.Windows.Forms.CheckBox();
+            this.eight = new System.Windows.Forms.CheckBox();
+            this.onetwentyeight = new System.Windows.Forms.CheckBox();
+            this.sixtyfour = new System.Windows.Forms.CheckBox();
+            this.thirtytwo = new System.Windows.Forms.CheckBox();
+            this.sixteen = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.senha = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.letra = new System.Windows.Forms.Label();
+            this.confirm = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.AutoSize = true;
+            this.one.Location = new System.Drawing.Point(55, 50);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(32, 17);
+            this.one.TabIndex = 0;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.CheckedChanged += new System.EventHandler(this.one_CheckedChanged);
+            // 
+            // two
+            // 
+            this.two.AutoSize = true;
+            this.two.Location = new System.Drawing.Point(55, 70);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(32, 17);
+            this.two.TabIndex = 1;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.CheckedChanged += new System.EventHandler(this.two_CheckedChanged);
+            // 
+            // four
+            // 
+            this.four.AutoSize = true;
+            this.four.Location = new System.Drawing.Point(55, 90);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(32, 17);
+            this.four.TabIndex = 2;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.CheckedChanged += new System.EventHandler(this.four_CheckedChanged);
+            // 
+            // eight
+            // 
+            this.eight.AutoSize = true;
+            this.eight.Location = new System.Drawing.Point(55, 110);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(32, 17);
+            this.eight.TabIndex = 3;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.CheckedChanged += new System.EventHandler(this.eight_CheckedChanged);
+            // 
+            // onetwentyeight
+            // 
+            this.onetwentyeight.AutoSize = true;
+            this.onetwentyeight.Location = new System.Drawing.Point(55, 190);
+            this.onetwentyeight.Name = "onetwentyeight";
+            this.onetwentyeight.Size = new System.Drawing.Size(44, 17);
+            this.onetwentyeight.TabIndex = 7;
+            this.onetwentyeight.Text = "128";
+            this.onetwentyeight.UseVisualStyleBackColor = true;
+            this.onetwentyeight.CheckedChanged += new System.EventHandler(this.onetwentyeight_CheckedChanged);
+            // 
+            // sixtyfour
+            // 
+            this.sixtyfour.AutoSize = true;
+            this.sixtyfour.Location = new System.Drawing.Point(55, 170);
+            this.sixtyfour.Name = "sixtyfour";
+            this.sixtyfour.Size = new System.Drawing.Size(38, 17);
+            this.sixtyfour.TabIndex = 6;
+            this.sixtyfour.Text = "64";
+            this.sixtyfour.UseVisualStyleBackColor = true;
+            this.sixtyfour.CheckedChanged += new System.EventHandler(this.sixtyfour_CheckedChanged);
+            // 
+            // thirtytwo
+            // 
+            this.thirtytwo.AutoSize = true;
+            this.thirtytwo.Location = new System.Drawing.Point(55, 150);
+            this.thirtytwo.Name = "thirtytwo";
+            this.thirtytwo.Size = new System.Drawing.Size(38, 17);
+            this.thirtytwo.TabIndex = 5;
+            this.thirtytwo.Text = "32";
+            this.thirtytwo.UseVisualStyleBackColor = true;
+            this.thirtytwo.CheckedChanged += new System.EventHandler(this.thirtytwo_CheckedChanged);
+            // 
+            // sixteen
+            // 
+            this.sixteen.AutoSize = true;
+            this.sixteen.Location = new System.Drawing.Point(55, 130);
+            this.sixteen.Name = "sixteen";
+            this.sixteen.Size = new System.Drawing.Size(38, 17);
+            this.sixteen.TabIndex = 4;
+            this.sixteen.Text = "16";
+            this.sixteen.UseVisualStyleBackColor = true;
+            this.sixteen.CheckedChanged += new System.EventHandler(this.sixteen_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Password:";
+            // 
+            // senha
+            // 
+            this.senha.AutoSize = true;
+            this.senha.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senha.Location = new System.Drawing.Point(115, 275);
+            this.senha.Name = "senha";
+            this.senha.Size = new System.Drawing.Size(0, 15);
+            this.senha.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(263, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Current Letter:";
+            // 
+            // letra
+            // 
+            this.letra.AutoSize = true;
+            this.letra.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letra.Location = new System.Drawing.Point(321, 103);
+            this.letra.Name = "letra";
+            this.letra.Size = new System.Drawing.Size(0, 49);
+            this.letra.TabIndex = 11;
+            // 
+            // confirm
+            // 
+            this.confirm.Location = new System.Drawing.Point(268, 190);
+            this.confirm.Name = "confirm";
+            this.confirm.Size = new System.Drawing.Size(160, 31);
+            this.confirm.TabIndex = 12;
+            this.confirm.Text = "Add Letter";
+            this.confirm.UseVisualStyleBackColor = true;
+            this.confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(394, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(41, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Username:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 247);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(464, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(26, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Cancel";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(502, 343);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.confirm);
+            this.Controls.Add(this.letra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.senha);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.onetwentyeight);
+            this.Controls.Add(this.sixtyfour);
+            this.Controls.Add(this.thirtytwo);
+            this.Controls.Add(this.sixteen);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.one);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "International Cool and Useful Passtyper (ICUP)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox one;
+        private System.Windows.Forms.CheckBox two;
+        private System.Windows.Forms.CheckBox four;
+        private System.Windows.Forms.CheckBox eight;
+        private System.Windows.Forms.CheckBox onetwentyeight;
+        private System.Windows.Forms.CheckBox sixtyfour;
+        private System.Windows.Forms.CheckBox thirtytwo;
+        private System.Windows.Forms.CheckBox sixteen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label senha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label letra;
+        private System.Windows.Forms.Button confirm;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
